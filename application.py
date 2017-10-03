026bff02b72dc1f7034656ba16ccfc3755224000
KeltarHelviett <keltarhelviett@gmail.com>
2017-10-03 23:18:52 +1000

    Restructured project

--- application.py
+++ application.py
@@ -1,1 +1,17 @@
-import PyQt5
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QDialog, QMainWindow
+from views.MainView import Ui_MainWindow
+
+if __name__ == '__main__':
+    
+    app = QApplication(sys.argv)
+
+    mainView = QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(mainView)
+
+    mainView.show()
+    
+    sys.exit(app.exec_())
